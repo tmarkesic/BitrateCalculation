3f156848250491b970683d35809aef7f13f62fed
tmarkesic <tmarkesic1@etf.unsa.ba>
2023-12-07 11:48:41 +0100

    Commit with small changes

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ using static System.Net.Mime.MediaTypeNames;
 
 namespace BitrateCalculation
 {
-    internal class Program
+    public class Program
     {
         static void Main()
         {
@@ -18,22 +18,29 @@ namespace BitrateCalculation
 
             //Reading from JSON file
             string jsonData = File.ReadAllText(jsonPath);
-
+            
             //Deserializing JSON string to class object 
             AristaData aristaData = JsonConvert.DeserializeObject<AristaData>(jsonData);
 
             // Defining polling rate in Hz
             int pollingRate = 2;
 
-            // Bitrate calculation for Tx and Rx using formula: 
-            // bitrate [bits per second]= number of bytes * 8 [bit] * polling rate [Hz = 1/s]  
-            var bitrateValueRx = aristaData.NIC[0].Rx * 8 * pollingRate;
-            var bitrateValueTx = aristaData.NIC[0].Tx * 8 * pollingRate;
+            // Bitrate calculation for Tx and Rx
+            var bitrateValueRx = CalculateBitrate(aristaData.NIC[0].Rx, pollingRate);
+            var bitrateValueTx = CalculateBitrate(aristaData.NIC[0].Tx, pollingRate);
             
             // Printing Bitrate value for Rx and Tx
             Console.WriteLine("Bitrate value on Rx: " + bitrateValueRx);
             Console.WriteLine("Bitrate value on Tx: " + bitrateValueTx);
 
+        }
+
+        static long CalculateBitrate(long dataAmount, int pollingRate)
+        {
+            // Calculate bitrate using the formula:
+            // bitrate[bits per second] = data amount * 8 [bits] * polling rate [Hz = 1/s]           
+            long bitrate = dataAmount * 8 * pollingRate;
+            return bitrate;
         }
     }
 }
